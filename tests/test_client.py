0ed00a8bc552cd780d5cf6982b5fe9d9c9fa982e
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2022-02-12 16:11:48 +0100

    Add tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,5 +11,11 @@ class SkinportTestCase(unittest.IsolatedAsyncioTestCase):
     async def test_get_items(self):
         await self.client.get_items()
 
+    async def test_get_sales_history(self):
+        await self.client.get_sales_history(market_hash_name=["Yeti Coated Wrench (Minimal Wear)"])
+
+    async def test_get_sales_out_of_stock(self):
+        await self.client.get_sales_out_of_stock()
+
     async def asyncTearDown(self):
         await self.client.close()
