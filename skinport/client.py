723659bd5aa7c7b7f5a082a6f604b85977a03314
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2022-02-12 13:51:53 +0100

    Add documentation

--- skinport/client.py
+++ skinport/client.py
@@ -23,8 +23,8 @@ SOFTWARE.
 """
 
 from typing import List
-from .enums import Currency
-from .errors import ParamRequired
+from .enums import AppID, Currency
+from .errors import ParamRequired, AuthenticationError
 
 from .http import HTTPClient
 from .sale import Sale
@@ -32,20 +32,54 @@ from .transaction import Transaction
 
 from .item import Item, ItemWithSales, ItemOutOfStock
 
+__all__ = ("Client",)
+
 
 class Client:
     def __init__(self):
         self.http: HTTPClient = HTTPClient()
+        self._closed = False
 
     async def login(self, client_id: str = None, client_secret: str = None):
         await self.http.set_token(client_id, client_secret)
 
     async def close(self) -> None:
+        """*coroutine*
+        Closes the `aiohttp.ClientSession`.
+        """
+        if self._closed:
+            return
+
+        self._closed = True
         await self.http.close()
 
     async def get_items(
-        self, *, app_id: int = 730, currency: Currency = "EUR", tradable: str = "false"
+        self,
+        *,
+        app_id: AppID = AppID.csgo,
+        currency: Currency = "EUR",
+        tradable: bool = False
     ) -> List[Item]:
+        """
+        Returns a :class:`list` of :class:`.Item`.
+
+        Parameters
+        ----------
+        app_id: :class:`.AppID`
+            The app_id for the inventory's game.
+            Defaults to ``730``.
+        currency: :class:`.Currency`
+            The currency for pricing.
+            Defaults to ``EUR``.
+        tradable: :class:`bool`
+            Whether or not to show only tradable items.
+            Defaults to ``False``.
+
+        Returns
+        -------
+        :class:`list` of :class:`.Item`
+        """
+
         params = {"app_id": app_id, "currency": currency, "tradable": tradable}
         data = await self.http.get_items(params=params)
         return [Item(data=item) for item in data]
@@ -77,6 +111,29 @@ class Client:
     async def get_account_transactions(
         self, *, page: int = 1, limit: int = 100, order: str = "desc"
     ) -> List[Transaction]:
+        """
+        Returns a :class:`list` of :class:`.Item`.
+
+        Parameters
+        ----------
+        page: :class:`int`
+            Pagination Page.
+            Defaults to ``1``.
+        limit: :class:`int`
+            Limit results between ``1`` and ``100``.
+            Defaults to ``100``.
+        order: :class:`str`
+            Order results by asc or desc.
+            Defaults to ``desc``.
+
+        Returns
+        -------
+        :class:`list` of :class:`.Item`
+
+        Raises
+        ------
+        :exception:`.AuthenticationError`
+        """
         params = {"page": page, "limit": limit, "order": order}
         data = await self.http.get_account_transactions(params=params)
-        return [Sale(data=sale) for sale in data]
+        return [Transaction(data=transaction) for transaction in data["data"]] if data else []
