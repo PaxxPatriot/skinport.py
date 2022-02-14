0e590b8eb19db47c812e39cb9ba61efe562b014d
PaxxPatriot <skinport.py@gmail.com>
2022-02-14 17:48:57 +0100

    Adjust client_example

--- examples/client_example.py
+++ examples/client_example.py
@@ -1,23 +1,19 @@
 import asyncio
-from collections import Counter
 
 import skinport
-from skinport.enums import AppID
+from skinport import AppID, Credit
 
 
 async def main():
     client = skinport.Client()
-    counter = Counter()
     try:
         transactions = await client.get_account_transactions(limit=10)
         for transaction in transactions:
-            print(transaction.type)
-            for item in transaction.items:
-                counter[item.buyer_country] += 1
+            if isinstance(transaction, Credit):
+                print(transaction.amount)
     except skinport.AuthenticationError:
         print("Authentication failed")
 
-    print(counter)
 
     items = await client.get_items(app_id=AppID.tf2)
     print(len(items))
