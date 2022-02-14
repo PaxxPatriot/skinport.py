d09a6323ed857ce22dcf58c4b8b3e5165b3f27ad
PaxxPatriot <skinport.py@gmail.com>
2022-02-14 18:58:24 +0100

    Version bump v0.2.0

--- skinport/__init__.py
+++ skinport/__init__.py
@@ -34,7 +34,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=0, minor=2, micro=0, releaselevel="alpha", serial=0
+    major=0, minor=2, micro=0, releaselevel="final", serial=0
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
