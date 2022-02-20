84ddeefca361e4bf061604b570623bc2e051350a
PaxxPatriot <skinport.py@gmail.com>
2022-02-20 03:22:21 +0100

    Add Python versions to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ import re
 
 version = ""
 with open("skinport/__init__.py") as f:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
-    ).group(1)
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError("version is not set")
@@ -37,6 +35,9 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries",
