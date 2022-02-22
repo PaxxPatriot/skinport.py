7a2cc296b4ce99b2f5f49553104838c0af9f9ef1
PaxxPatriot <skinport.py@gmail.com>
2022-02-22 19:25:27 +0100

    Adjust websocket_example

--- examples/websocket_example.py
+++ examples/websocket_example.py
@@ -3,8 +3,18 @@ import skinport
 client = skinport.Client()
 
 
-@client.event
+@client.listen("saleFeed")
 async def on_sale_feed(data):
+    print(data)
+
+
+@client.listen("maintenanceUpdated")
+async def on_maintenance_updated(data):
+    print(data)
+
+
+@client.listen("steamStatusUpdated")
+async def on_steam_status_updated(data):
     print(data)
 
 
