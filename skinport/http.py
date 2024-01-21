0d1e763d1b1daee0f437bede44f23f84fa513f65
PaxxPatriot <skinport.py@gmail.com>
2024-01-21 11:46:36 +0100

    Add ratelimit_lock

--- skinport/http.py
+++ skinport/http.py
@@ -63,6 +63,7 @@ class HTTPClient:
         self.auth = None
         self.proxy: Optional[str] = proxy
         self.proxy_auth: Optional[aiohttp.BasicAuth] = proxy_auth
+        self.ratelimit_lock: asyncio.Lock = asyncio.Lock()
 
         user_agent = "skinport.py {0}) Python/{1[0]}.{1[1]} aiohttp/{2}"
         self.user_agent: str = user_agent.format(__version__, sys.version_info, str(aiohttp.__version__))  #
@@ -93,33 +94,35 @@ class HTTPClient:
         if params:
             kwargs["params"] = params
 
-        for _ in range(2):
-            async with self.__session.request(method, url, auth=self.auth, **kwargs) as response:
-                _log.debug(f"{method} {url} with {kwargs} has returned {response.status}")
+        async with self.ratelimit_lock:
+            for _ in range(2):
+                async with self.__session.request(method, url, auth=self.auth, **kwargs) as response:
+                    _log.debug(f"{method} {url} with {kwargs} has returned {response.status}")
 
-                data = await response.json()
+                    data = await response.json()
 
-                if 300 > response.status >= 200:
-                    _log.debug(f"{method} {url} has received {data}")
-                    return data
+                    if 300 > response.status >= 200:
+                        _log.debug(f"{method} {url} has received {data}")
+                        return data
 
-                if response.status in {500, 503}:
-                    raise InternalServerError(response, data)
+                    if response.status in {500, 503}:
+                        raise InternalServerError(response, data)
 
-                if response.status == 401:
-                    raise AuthenticationError(response, data)
-                if response.status == 402:
-                    raise InsufficientFunds(response, data)
-                if response.status == 403:
-                    raise InvalidScope(response, data)
-                if response.status == 404:
-                    raise NotFound(response, data)
-                if response.status == 429:
-                    # We are getting rate-limited, read retry-after header and try again
-                    retry_after = int(headers.get('Retry-After', 60))
-                    await asyncio.sleep(retry_after)
-                    continue
-                raise HTTPException(response, data)
+                    if response.status == 401:
+                        raise AuthenticationError(response, data)
+                    if response.status == 402:
+                        raise InsufficientFunds(response, data)
+                    if response.status == 403:
+                        raise InvalidScope(response, data)
+                    if response.status == 404:
+                        raise NotFound(response, data)
+                    if response.status == 429:
+                        # We are getting rate-limited, read retry-after header and try again
+                        retry_after = int(headers.get('Retry-After', 60))
+                        _log.debug(f"{method} {url} is getting rate-limited, retry after {retry_after} seconds")
+                        await asyncio.sleep(retry_after)
+                        continue
+                    raise HTTPException(response, data)
 
     async def get_items(self, **parameters: Any) -> List[Dict[str, Any]]:
         return await self.request(Route("GET", "/items"), **parameters)
