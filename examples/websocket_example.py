4c2843a1f6195c75edf07f88952473189af501a4
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2024-12-07 15:13:02 +0100

    Update simple WebSocket example
    Feed websocket data into skinport.SaleFeed to easily access attributes

--- examples/websocket_example.py
+++ examples/websocket_example.py
@@ -5,7 +5,12 @@ client = skinport.Client()
 
 @client.listen("saleFeed")
 async def on_sale_feed(data):
-    print(data)
+    salefeed = skinport.SaleFeed(data=data)
+    sales = salefeed.sales
+
+    print(f"Event Type: {salefeed.event_type}")
+    # For more attributes check out the documentation: https://paxxpatriot.github.io/skinport.py/api.html#skinport.SaleFeedSale
+    print(f"Market Hash Names: {", ".join([sale.market_hash_name for sale in sales])}")
 
 
 @client.listen("maintenanceUpdated")
