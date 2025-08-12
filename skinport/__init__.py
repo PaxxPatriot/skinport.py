25739ecca58934f110e70a34067e0c074fedc59f
PaxxPatriot <skinport.py@gmail.com>
2025-08-12 23:43:28 +0200

    Bump version v0.33.0

--- skinport/__init__.py
+++ skinport/__init__.py
@@ -10,7 +10,7 @@ __title__ = "skinport"
 __author__ = "PaxxPatriot"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 PaxxPatriot"
-__version__ = "0.32.0"
+__version__ = "0.33.0"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -37,6 +37,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=32, micro=0, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=0, minor=33, micro=0, releaselevel="final", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
